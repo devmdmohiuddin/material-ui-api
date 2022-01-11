460432c86113fa0b187b82e8a7c0b1a78dab7be5
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-01-11 10:24:52 +0600

    setup material ui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CardTest from './components/CardTest';
+
+const App = () => {
+  return (
+    <div className="App">
+      <CardTest />
+    </div>
+  );
+}
+
+export default App;
